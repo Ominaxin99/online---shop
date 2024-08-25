432b38bd87a05a3b85a130812294782c5804cb3a
missinsta <missinsta303gmail.com>
2024-08-25 14:52:09 +0500

    massif honalarini anglatuvshi formula

--- SRC/Main.java
+++ SRC/Main.java
@@ -1,8 +1,25 @@
 package SRC;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("OMINA");
+        System.out.print("Massiv uzunligini kiriting: ");
+        int sizeee = scanner.nextInt();
+        int[] array = new int[sizeee];
+
+        for (int i = 0; i < sizeee; i++) {
+            System.out.print("[" + i + "] = ");
+            array[i] = scanner.nextInt();
+        }
+        //////////////////
+        int summa  = 0;
+        for (int i = 0; i < sizeee; i++) {
+            if (array[i] % 2 != 0) summa++;
+            else summa--;
+        }
+        System.out.println(summa==0);
 
     }
 }
